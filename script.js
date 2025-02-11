66b16e27178977be3242b224541b327cf417f657
SarikViper <dzamolovsardor5@gmail.com>
2025-02-11 17:42:13 +0500

    added selectors

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const openModalBtn = document.querySelector('#openModal')
 const closeModal = document.getElementById('closeModal');
 const modal = document.getElementById('modal');
 const modalContent = document.querySelector(".modal-content");
+const input = document.querySelector(".task-input");
+const select = document.querySelector("#task_type");
+const select2 = document.querySelector("#task_dificulty");
+const date = document.querySelector('.task-date');
+const note = document.querySelector(".task-notes");
+const btnadd = document.querySelector('.submit-task')
 
 openModalBtn.onclick = () => {
     modal.classList.remove('hidden');
@@ -16,3 +22,4 @@ document.onclick = (e) => {
         modal.classList.add('hidden');
     }
 }
+
