01857752531988f0dfe8d0ba1b23032bc24c1a8e
SarikViper <dzamolovsardor5@gmail.com>
2025-02-15 18:48:06 +0500

    -

--- js/db.js
+++ js/db.js
@@ -1,2 +1,16 @@
-const todoCards = [
-];
+const todoCards = JSON.parse(localStorage.getItem("tasks")) ?? [];
+
+console.log(todoCards);
+
+
+// Nullish operator for true or false you can use this operator to "||""
+//??
+
+// JS to JSON
+// console.log(JSON.stringify(todoCards));
+// console.log(JSON.parse(JSON.stringify(todoCards)));
+
+
+// get from localStorage and parsing
+// const todos = JSON.parse(localStorage.getItem("tasks"));
+// console.log(todos);
